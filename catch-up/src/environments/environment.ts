2f68644dd319999eed79ef496e78f0aaa03a4078
Marcelo <c3sv.19@gmail.com>
2026-04-28 11:44:26 -0500

    feat(logo): configure logo provider environment settings for development and production.

--- catch-up/src/environments/environment.ts
+++ catch-up/src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production:false,
+  logoProvideApiBaseUrl: 'https://img.logo.dev/',
+  logoProviderPublishablekey:'pk_LLVI5sWpTryEERcWHpH5mg'
+};
